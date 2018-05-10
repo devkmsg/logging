949fafee8d177f3bd1de8bc7fd4989729dc1e20d
Andrew Thompson <netengr2009@gmail.com>
2018-05-10 05:20:29 +0000

    Adding base sg

---
+++ terraform/security_groups.tf
@@ -0,0 +1,9 @@
+module "base_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "base"
+  description = "Base Security group"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress_cidr_blocks = ["76.119.218.144/32"] # FIXME: set this dynamic
+}
