001734b76a19ef1cc8664d355ec3c0ed98bdbef2
Andrew Thompson <netengr2009@gmail.com>
2018-05-15 14:37:43 +0000

    Add security group

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -7,4 +7,17 @@ module "base_sg" {
 
   ingress_cidr_blocks = ["76.119.218.144/32"] # FIXME: set this dynamic
   ingress_rules       = ["ssh-tcp"]
+
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_ipv6_cidr_blocks = []
+  egress_rules = ["dns-udp", "dns-tcp", "https-443-tcp"]
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 123
+      to_port     = 123
+      protocol    = "udp"
+      description = "NTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
 }
