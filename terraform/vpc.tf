1e7d227d436a2f773659428a15af6b405e80cb21
Andrew Thompson <netengr2009@gmail.com>
2018-05-15 14:35:48 +0000

    Add acl rules that can be iterated on

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,8 +7,42 @@ module "vpc" {
   azs            = ["us-east-1a", "us-east-1b"]
   public_subnets = ["10.0.0.0/28", "10.0.0.16/28"]
 
+  enable_s3_endpoint = true
+
   tags = {
     Terraform   = "true"
     Environment = "dev"
   }
+}
+
+resource "aws_network_acl" "public" {
+  vpc_id = "${module.vpc.vpc_id}"
+  subnet_ids = ["${module.vpc.public_subnets}"]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_default_network_acl" "default" {
+  default_network_acl_id = "${module.vpc.default_network_acl_id}"
+}
+
+resource "aws_network_acl_rule" "all-in" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 100
+  egress         = false
+  protocol       = "all"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "all-out" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 100
+  egress         = true
+  protocol       = "all"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
 }
